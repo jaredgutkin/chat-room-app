15df86d8fd9bcba7f22b0198a97c9e249bf8d456
Jared Gutkin <jgutkinsam@gmail.com>
2022-06-27 11:57:07 -0500

    socket.io implemeted

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
+
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
+
+//run when clent connects
+io.on('connection', socket => {
+    console.log('New wS connection')
+})
+
 const PORT = 3000 || process.env.PORT
 
-app.listen(PORT, ()=>{
+server.listen(PORT, ()=>{
     console.log(`server running on port ${PORT}`)
 })
