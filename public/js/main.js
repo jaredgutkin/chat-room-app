8bc79a6417df8b481f407e38e6c538c24f09c737
Jared Gutkin <jgutkinsam@gmail.com>
2022-06-27 13:00:54 -0500

    submit message user time to dom

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
 const chatForm = document.getElementById('chat-form')
 const chatMessages = document.querySelector('.chat-messages')
 
+//get username and room from url
+
 const socket = io()
 
 //message from server
@@ -33,9 +35,9 @@ function outputMessage(message){
     const div = document.createElement('div')
     div.classList.add('message')
     div.innerHTML = `
-    <p class="meta">Brad <span>9:12pm</span></p>
+    <p class="meta">${message.username} <span>${message.time}</span></p>
     <p class="text">
-        ${message}
+        ${message.text}
     </p>`
     document.querySelector('.chat-messages').appendChild(div)
 }
