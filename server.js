507bec77bbfdffdda9833787274069c43eeac654
Jared Gutkin <jgutkinsam@gmail.com>
2022-06-27 12:06:20 -0500

    socket backend implementation

--- server.js
+++ server.js
@@ -14,7 +14,17 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 //run when clent connects
 io.on('connection', socket => {
-    console.log('New wS connection')
+
+    //welcome current user
+    socket.emit('message', 'Welcome to Chatcord')
+
+    //Broadcast when user connects
+    socket.broadcast.emit('message', 'A user has joined the chat')
+
+    //runs when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'a user has left the chat')
+    })
 })
 
 const PORT = 3000 || process.env.PORT
