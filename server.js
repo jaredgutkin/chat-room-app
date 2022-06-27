52a0b7394683c64810ce7aeeca92633aa38de947
Jared Gutkin <jgutkinsam@gmail.com>
2022-06-27 12:53:49 -0500

    util and format messages added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const formatMessage = require('./utils/messages')
 
 
 const app = express()
@@ -11,24 +12,26 @@ const io = socketio(server)
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
+const botName = 'Hammy'
+
 
 //run when clent connects
 io.on('connection', socket => {
 
     //welcome current user
-    socket.emit('message', 'Welcome to Chatcord')
+    socket.emit('message', formatMessage(botName, 'Welcome to Chatcord'))
 
     //Broadcast when user connects
-    socket.broadcast.emit('message', 'A user has joined the chat')
+    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'))
 
     //runs when client disconnects
     socket.on('disconnect', () => {
-        io.emit('message', 'a user has left the chat')
+        io.emit('message', formatMessage(botName, 'a user has left the chat'))
     })
 
     //listen for chat message
     socket.on('chatMessage', (msg) =>{
-        io.emit('message', msg)
+        io.emit('message', formatMessage('USER', msg))
     })
 })
 
